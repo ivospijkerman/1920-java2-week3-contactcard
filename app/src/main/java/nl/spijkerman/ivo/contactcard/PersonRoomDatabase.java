0d9cd764f94b524bdbe36d2a25f767407cef737e
Ivo Spijkerman <ivo@spijkerman.nl>
2019-11-26 16:48:34 +0100

    added repo

--- app/src/main/java/nl/spijkerman/ivo/contactcard/PersonRoomDatabase.java
+++ app/src/main/java/nl/spijkerman/ivo/contactcard/PersonRoomDatabase.java
@@ -6,12 +6,24 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Database(entities = {Person.class}, version = 1, exportSchema = false)
 public abstract class PersonRoomDatabase extends RoomDatabase {
 
     public abstract PersonDao personDao();
 
     private static volatile PersonRoomDatabase INSTANCE;
+
+    private static final int NUMBER_OF_THREADS = 4;
+    private static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static void execute(Runnable action) {
+        databaseWriteExecutor.execute(action);
+    }
+
 
     static PersonRoomDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
@@ -26,4 +38,6 @@ public abstract class PersonRoomDatabase extends RoomDatabase {
         }
         return INSTANCE;
     }
+
+
 }
