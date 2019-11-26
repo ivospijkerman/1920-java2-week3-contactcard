2c58bb3356cf5606b22b5a44c9e282b5e368a800
Ivo Spijkerman <ivo@spijkerman.nl>
2019-11-26 17:08:33 +0100

    added callback

--- app/src/main/java/nl/spijkerman/ivo/contactcard/PersonRoomDatabase.java
+++ app/src/main/java/nl/spijkerman/ivo/contactcard/PersonRoomDatabase.java
@@ -2,15 +2,31 @@ package nl.spijkerman.ivo.contactcard;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Database(entities = {Person.class}, version = 1, exportSchema = false)
 public abstract class PersonRoomDatabase extends RoomDatabase {
+
+    private static Callback initialLoad = new Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+
+            execute(() -> {
+                PersonDao dao = INSTANCE.personDao();
+
+                Person p = new Person("Harry", "Gaastra");
+                dao.insert(p);
+            });
+        }
+    };
 
     public abstract PersonDao personDao();
 
@@ -32,6 +48,7 @@ public abstract class PersonRoomDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             PersonRoomDatabase.class,
                             "contactCard")
+                            .addCallback(initialLoad)
                             .build();
                 }
             }
