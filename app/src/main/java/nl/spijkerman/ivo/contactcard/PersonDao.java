25c5b1986effd431dcfaf5fabf1944ce4f5337ad
Ivo Spijkerman <ivo@spijkerman.nl>
2019-11-26 16:21:55 +0100

    added DAO

---
+++ app/src/main/java/nl/spijkerman/ivo/contactcard/PersonDao.java
@@ -0,0 +1,25 @@
+package nl.spijkerman.ivo.contactcard;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface PersonDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Person person);
+
+    @Query("DELETE FROM people")
+    void deleteAll();
+
+    @NonNull
+    @Query("SELECT * FROM people ORDER BY firstName")
+    List<Person> getAllPeople();
+
+}
